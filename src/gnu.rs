messense/implib-rs

commit e8e93eecedf16cba6c8bf4872446b1d5114c7d4a

Bump object to 0.36.4 (#12)
And refactor the Relocation instantiation.

src/gnu.rs
@@ -1,10 +1,9 @@
 use std::io::{Error, ErrorKind, Seek, Write};
 
 use object::pe::*;
-use object::write::{Object, Relocation, Symbol, SymbolSection};
+use object::write::{Object, Relocation, Symbol, SymbolId, SymbolSection};
 use object::{
-    BinaryFormat, Endianness, RelocationEncoding, RelocationKind, SectionFlags, SectionKind,
-    SymbolFlags, SymbolKind, SymbolScope,
+    BinaryFormat, Endianness, SectionFlags, SectionKind, SymbolFlags, SymbolKind, SymbolScope,
 };
 
 use crate::def::{ModuleDef, ShortExport};
@@ -109,7 +108,20 @@ impl<'a> ObjectFactory<'a> {
             seq: 0,
         })
     }
-
+    fn make_relocation(
+        &self,
+        offset: u64,
+        symbol: SymbolId,
+        addend: i64,
+        rel_kind: u16,
+    ) -> Relocation {
+        Relocation {
+            offset,
+            symbol,
+            addend,
+            flags: object::RelocationFlags::Coff { typ: rel_kind },
+        }
+    }
     fn make_head(&self) -> Result<ArchiveMember, Error> {
         let mut obj = Object::new(
             BinaryFormat::Coff,
@@ -159,30 +171,11 @@ impl<'a> ObjectFactory<'a> {
         obj.add_file_symbol(b"fake".to_vec());
         let id5_sym = obj.section_symbol(id5);
         let id4_sym = obj.section_symbol(id4);
-        obj.add_relocation(
-            id2,
-            Relocation {
-                offset: 0,
-                size: 0,
-                kind: RelocationKind::Coff(self.machine.img_rel_relocation()),
-                encoding: RelocationEncoding::Generic,
-                symbol: id4_sym,
-                addend: 0,
-            },
-        )
-        .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
-        obj.add_relocation(
-            id2,
-            Relocation {
-                offset: 16,
-                size: 0,
-                kind: RelocationKind::Coff(self.machine.img_rel_relocation()),
-                encoding: RelocationEncoding::Generic,
-                symbol: id5_sym,
-                addend: 0,
-            },
-        )
-        .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
+        let img_rel = self.machine.img_rel_relocation();
+        obj.add_relocation(id2, self.make_relocation(0, id4_sym, 0, img_rel))
+            .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
+        obj.add_relocation(id2, self.make_relocation(16, id5_sym, 0, img_rel))
+            .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
 
         let import_name = self.import_name.replace('.', "_");
 
@@ -212,18 +205,8 @@ impl<'a> ObjectFactory<'a> {
         let iname_sym_id = obj.add_symbol(iname_sym);
 
         obj.append_section_data(id2, &[0; 20], 4);
-        obj.add_relocation(
-            id2,
-            Relocation {
-                offset: 12,
-                size: 0,
-                kind: RelocationKind::Coff(self.machine.img_rel_relocation()),
-                encoding: RelocationEncoding::Generic,
-                symbol: iname_sym_id,
-                addend: 0,
-            },
-        )
-        .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
+        obj.add_relocation(id2, self.make_relocation(12, iname_sym_id, 0, img_rel))
+            .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
         Ok(ArchiveMember {
             name: format!("{}_h.o", self.output_name.replace('.', "_")),
             data: obj
@@ -423,31 +406,16 @@ impl<'a> ObjectFactory<'a> {
             obj.append_section_data(text_sec, jmp_stub, 4);
             obj.add_relocation(
                 text_sec,
-                Relocation {
-                    offset,
-                    size: 0,
-                    kind: RelocationKind::Coff(rel_kind),
-                    encoding: RelocationEncoding::Generic,
-                    symbol: exp_imp_sym,
-                    addend: 0,
-                },
+                self.make_relocation(offset, exp_imp_sym, 0, rel_kind),
             )
             .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
         }
 
+        let img_rel = self.machine.img_rel_relocation();
+
         obj.append_section_data(id7, &[0; 4], 4);
-        obj.add_relocation(
-            id7,
-            Relocation {
-                offset: 0,
-                size: 0,
-                kind: RelocationKind::Coff(self.machine.img_rel_relocation()),
-                encoding: RelocationEncoding::Generic,
-                symbol: head_sym,
-                addend: 0,
-            },
-        )
-        .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
+        obj.add_relocation(id7, self.make_relocation(0, head_sym, 0, img_rel))
+            .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
 
         let id6_sym = obj.section_symbol(id6);
         let id5_data = if export.no_name {
@@ -462,18 +430,8 @@ impl<'a> ObjectFactory<'a> {
                 0x80,
             ]
         } else {
-            obj.add_relocation(
-                id5,
-                Relocation {
-                    offset: 0,
-                    size: 0,
-                    kind: RelocationKind::Coff(self.machine.img_rel_relocation()),
-                    encoding: RelocationEncoding::Generic,
-                    symbol: id6_sym,
-                    addend: 0,
-                },
-            )
-            .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
+            obj.add_relocation(id5, self.make_relocation(0, id6_sym, 0, img_rel))
+                .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
             [0; 8]
         };
         obj.append_section_data(id5, &id5_data, 4);
@@ -490,18 +448,8 @@ impl<'a> ObjectFactory<'a> {
                 0x80,
             ]
         } else {
-            obj.add_relocation(
-                id4,
-                Relocation {
-                    offset: 0,
-                    size: 0,
-                    kind: RelocationKind::Coff(self.machine.img_rel_relocation()),
-                    encoding: RelocationEncoding::Generic,
-                    symbol: id6_sym,
-                    addend: 0,
-                },
-            )
-            .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
+            obj.add_relocation(id4, self.make_relocation(0, id6_sym, 0, img_rel))
+                .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
             [0; 8]
         };
         obj.append_section_data(id4, &id4_data, 4);

Cargo.toml
@@ -11,7 +11,7 @@ readme = "README.md"
 
 [dependencies]
 memoffset = "0.9.0"
-object = { version = "0.32.1", default-features = false, features = ["pe"] }
+object = { version = "0.36.4", default-features = false, features = ["pe"] }
 
 [features]
 default = ["msvc", "gnu"]

commit 52b9bbdb6af2ab316309fcbf6fc61768ecd53d27

Fix GNU import library generation
After commit gimli-rs/object@6dea0d591a38acc11d833f1aff2da711e55edc59,
the relocation addend is now adjusted based on the relocation flags.
Update our handling to properly account for this change.

Regressed since commit e8e93eecedf16cba6c8bf4872446b1d5114c7d4a.

src/gnu.rs
@@ -409,7 +409,7 @@ impl<'a> ObjectFactory<'a> {
             obj.append_section_data(text_sec, jmp_stub, 4);
             obj.add_relocation(
                 text_sec,
-                self.make_relocation(offset, exp_imp_sym, 0, rel_kind),
+                self.make_relocation(offset, exp_imp_sym, -4, rel_kind),
             )
             .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
         }